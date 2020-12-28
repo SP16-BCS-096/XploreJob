ca95eac6c005fffb48c73dcf8c68fae7e0b01f01
SP16-BCS-096 <69343165+SP16-BCS-096@users.noreply.github.com>
2020-12-28 08:29:28 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AdminDashboard from "./components/Admin/AdminDashboard";
-import CandidateDashboard from "./components/Candidate/CandidateDashboard";
+
 
 import AdminLogin from "./components/Admin/AdminLogin";
 import CandidateLogin from "./components/Candidate/CandidateLogin";
 import RecruiterLogin from "./components/Recruiter/RecruiterLogin";
 
 import LSogout from "./components/Candidate/Logout";
-import MainMenu from "./components/Candidate/MainMenu";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -20,7 +19,7 @@ import Toolbar from"./components/Toolbar/Toolbar";
 
 import JobCreate from "./components/Recruiter/JobCreate";
 import CvList from "./components/Recruiter/CvList";
-
+import ViewJobsList from "./components/Admin/ViewJobsList";
 import JobsList from "./components/Candidate/JobsList";
 import RecruiterSignup from "./components/Recruiter/RecruiterSignup";
 import CandidateSignup from "./components/Candidate/CandidateSignup";
@@ -38,17 +37,18 @@ class App extends Component {
           <Route path="/CandidateSignup" component={CandidateSignup} />
           <Route path="/RecruiterSignup" component={RecruiterSignup} />
           <Route path="/AdminDashboard" component={AdminDashboard} />
-          <Route path="/CandidateDashboard" component={CandidateDashboard} />
+         
           <Route path="/AdminLogin" component={AdminLogin} />
           <Route path="/CandidateLogin" component={CandidateLogin} />
           <Route path="/RecruiterLogin" component={RecruiterLogin} />
           <Route path="/JobCreate" component={JobCreate} />
           <Route path="/JobsList" component={JobsList} />
+          <Route path="/ViewJobsList" component={ViewJobsList} />
           <Route path="/Cv" component={Cv} />
           <Route path="/CvList" component={CvList} />
-          <Route path="/MainMenu" component={MainMenu} />
-           <Route path="/Logout" component={Logout} />
-           <Route path="/Toolbar" component={Toolbar} />
+        
+          <Route path="/Logout" component={Logout} />
+          <Route path="/Toolbar" component={Toolbar} />
 
 
            
