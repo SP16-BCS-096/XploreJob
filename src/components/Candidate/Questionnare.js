4d6c6ee86357e3bdf877e7ee2c214500f7039c90
SP16-BCS-096 <69343165+SP16-BCS-096@users.noreply.github.com>
2021-01-03 21:30:19 +0500

    Add files via upload

---
+++ src/components/Candidate/Questionnare.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Survey from "survey-react";
+
+
+import "survey-react/survey.css";
+
+function Questionnare() {
+  let json = {
+    "pages": [
+      {
+       name: "page1",
+      elements: [
+       {
+     "type": "text",
+     "name": "question1",
+     "title": "Your Name",
+     "isRequired": true
+    },
+    {
+     "type": "text",
+     "name": "question2",
+     "title": "Email",
+     "isRequired": true
+    },
+        {
+         type: "comment",
+         name: "question3",
+         title: "Tell us Something About u",
+         isRequired: true
+        },
+        
+        {
+         type: "comment",
+         name: "question4",
+         title: "How did you learn about this platform?",
+         isRequired: true
+        },
+        {
+         type: "comment",
+         name: "question5",
+         title: "List all software you have used that’s related to your field.",
+         isRequired: true
+        },
+       
+        {
+         type: "comment",
+         name: "question6",
+         title: "What are your salary expectations?",
+         isRequired: true
+        }
+       ]
+      }
+     ]
+  };
+  
+  var surveyRender = <Survey.Survey json={json} />;
+  return (
+    <div className="Sarvey">
+      <h2>Questionnare</h2>
+      {surveyRender}
+    </div>
+  );
+}
+
+export default Questionnare;
